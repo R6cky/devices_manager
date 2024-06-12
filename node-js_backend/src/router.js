7e0fc1de80eed823a583a5f9de0456815f4420b7
r6cky <cleitonrocha321@hotmail.com>
2024-06-12 15:19:25 -0300

    feat: create computer logic router - create, list and list by id

--- node-js_backend/src/router.js
+++ node-js_backend/src/router.js
@@ -4,7 +4,9 @@ import { GetAllBluebirdsController } from "./controllers/bluebird/getAllBluebird
 import {GetBluebirdByIdController} from "./controllers/bluebird/getBluebirdByIdController.js";
 import {UpdateBluebirdController} from "./controllers/bluebird/updateBluebirdController.js";
 import {DeleteBluebirdController} from "./controllers/bluebird/deleteBluebirdController.js";
-
+import { CreateComputerController } from "./controllers/computer/createComputerController.js";
+import { GetAllComputerController } from "./controllers/computer/getAllComputerController.js";
+import { GetComputerByIdController } from "./controllers/computer/getComputerByIdController.js";
 
 const router = Router();
 
@@ -14,12 +16,29 @@ const getBluebirdById = new GetBluebirdByIdController();
 const updateBluebird = new UpdateBluebirdController();
 const deleteBlueBird = new DeleteBluebirdController();
 
+const createComputer = new CreateComputerController();
+const getAllComputer = new GetAllComputerController();
+const getComputerById = new GetComputerByIdController();
+// const updateComputer = new UpdateBluebirdController();
+// const deleteComputer = new DeleteBluebirdController();
+
+
+
+
+
 
 router.post("/bluebird", createBluebirds.handle);
 router.get("/bluebird", getAllBluebirds.handle);
 router.get("/bluebird/:id", getBluebirdById.handle);
 router.patch("/bluebird/:id", updateBluebird.handle);
 router.delete("/bluebird/:id", deleteBlueBird.handle);
+
+router.post("/computer", createComputer.handle);
+router.get("/computer", getAllComputer.handle);
+router.get("/computer/:id", getComputerById.handle);
+// router.patch("/computer/:id", updateComputer.handle);
+// router.delete("/computer/:id", deleteComputer.handle);
+
 
 
 
