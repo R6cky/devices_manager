fd4d289ab482424275a47374360d708eaa8ef41a
r6cky <cleitonrocha321@hotmail.com>
2024-06-20 20:17:44 -0300

    feat: create resource cors

--- node-js_backend/src/app.js
+++ node-js_backend/src/app.js
@@ -1,12 +1,13 @@
 import {router} from './router.js';
 import express from 'express';
+import cors from 'cors'
+
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
-
-
 
 
 app.get('/bluebirds', (req, res) => {   
