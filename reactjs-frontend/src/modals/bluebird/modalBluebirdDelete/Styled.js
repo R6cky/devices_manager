8339e593383be2b6e2f86ece6f220c07f674593d
r6cky <cleitonrocha321@hotmail.com>
2024-07-02 16:51:00 -0300

    rename modal bluebird delete file

--- reactjs-frontend/src/modals/bluebird/modalBluebirdDelete/Styled.js
+++ reactjs-frontend/src/modals/bluebird/modalBluebirdDelete/Styled.js
@@ -3,6 +3,15 @@ import styled from "styled-components";
 
 export const ModalBluebirdEditStyle = styled.div`
 
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+    border: solid green;
+    padding-top: 30px;
+    background-color: rgba(0,0,23,0.9) ;
+
 .form-bluebird{
         border: solid red;
         width: 400px;
