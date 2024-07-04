cffbf926a278ac5e1805308b5102ff4b601cdfa6
r6cky <cleitonrocha321@hotmail.com>
2024-07-04 13:46:11 -0300

    adjust layout of text-ara in bluebird item

--- reactjs-frontend/src/pages/bluebird/bluebirdList/bluebirdItem/BluebirdItem.jsx
+++ reactjs-frontend/src/pages/bluebird/bluebirdList/bluebirdItem/BluebirdItem.jsx
@@ -8,10 +8,18 @@ export const BluebirdItem = ({ bluebird }) => {
   return (
     <BluebirdItemStyled>
       <div className="item-content">
-        <h3>Serial Number: {bluebird.serial_name} </h3>
-        <h3>Hostname: {bluebird.host_name} </h3>
-        <h3>Data de reparo: {bluebird.fix_date} </h3>
-        <h3>Descrição: {bluebird.description} </h3>
+        <h3 className="item-content-serialname">
+          Serial Number: {bluebird.serial_name}{" "}
+        </h3>
+        <h3 className="item-content-hostname">
+          Hostname: {bluebird.host_name}{" "}
+        </h3>
+        <h3 className="item-content-fixdate">
+          Data de reparo: {bluebird.fix_date}{" "}
+        </h3>
+        <p className="item-content-description">
+          Descrição: {bluebird.description}{" "}
+        </p>
       </div>
       <div className="item-buttons">
         <button onClick={() => openModalEdit(bluebird)} className="edit">
