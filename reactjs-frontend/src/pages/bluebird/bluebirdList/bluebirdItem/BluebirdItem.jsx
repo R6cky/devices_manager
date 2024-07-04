a428a80b47159ec11af28b9b29458a28fc3ab98f
r6cky <cleitonrocha321@hotmail.com>
2024-07-04 10:05:59 -0300

    call openModalEdit function  in bluebird component

--- reactjs-frontend/src/pages/bluebird/bluebirdList/bluebirdItem/BluebirdItem.jsx
+++ reactjs-frontend/src/pages/bluebird/bluebirdList/bluebirdItem/BluebirdItem.jsx
@@ -1,25 +1,29 @@
-import { BluebirdItemStyled } from "./Styled"
-import { BluebirdContext } from "../../../../context/BluebirdContext"
-import { useContext } from "react"
+import { BluebirdItemStyled } from "./Styled";
+import { BluebirdContext } from "../../../../context/BluebirdContext";
+import { useContext } from "react";
 
+export const BluebirdItem = ({ bluebird }) => {
+  const { openModalDelete, openModalEdit } = useContext(BluebirdContext);
 
-
-export const BluebirdItem = ({bluebird}) => {
-
-    const {openModalDelete, openModalEdit} = useContext(BluebirdContext)
-
-    return (
-        <BluebirdItemStyled>
-            <div className="item-content">
-                <h3 >Serial Number: {bluebird.serial_name} </h3>
-                <h3 >Hostname: {bluebird.host_name} </h3>
-                <h3 >Data de reparo: {bluebird.fix_date} </h3>
-                <h3 >Descrição: {bluebird.description} </h3>
-            </div>
-            <div className="item-buttons">
-                <button onClick={()=> openModalEdit(bluebird.id)}  className="edit">Editar</button>
-                <button onClick={()=>openModalDelete(bluebird.id)} className="remove">remover</button>
-            </div>
-        </BluebirdItemStyled>
-            )
-        }
+  return (
+    <BluebirdItemStyled>
+      <div className="item-content">
+        <h3>Serial Number: {bluebird.serial_name} </h3>
+        <h3>Hostname: {bluebird.host_name} </h3>
+        <h3>Data de reparo: {bluebird.fix_date} </h3>
+        <h3>Descrição: {bluebird.description} </h3>
+      </div>
+      <div className="item-buttons">
+        <button
+          onClick={() => openModalEdit(bluebird.id, bluebird)}
+          className="edit"
+        >
+          Editar
+        </button>
+        <button onClick={() => openModalDelete(bluebird.id)} className="remove">
+          remover
+        </button>
+      </div>
+    </BluebirdItemStyled>
+  );
+};
