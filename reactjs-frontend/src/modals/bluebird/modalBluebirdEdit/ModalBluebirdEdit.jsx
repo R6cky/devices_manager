2b6ee459d6bb56708768839abef8a74ae670497d
r6cky <cleitonrocha321@hotmail.com>
2024-07-03 22:54:56 -0300

    exclude component of modal bluebird edit

--- reactjs-frontend/src/modals/bluebird/modalBluebirdEdit/ModalBluebirdEdit.jsx
+++ reactjs-frontend/src/modals/bluebird/modalBluebirdEdit/ModalBluebirdEdit.jsx
@@ -7,11 +7,7 @@ export const ModalBluebirdDelete = () => {
 
     return(
             <ModalBluebirdEditStyle>
-                <div className="header-delete">
-                   <p>Deseja excluir o bluebird? </p>
-                </div>
-                <button className="btn-delete-yes">Sim</button>
-                <button className="btn-delete-no">Não</button>
+                
             </ModalBluebirdEditStyle>
             
     )
