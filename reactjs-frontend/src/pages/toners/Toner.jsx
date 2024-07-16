e4c3b717003fb77d07a48388440c9dba4e344fda
r6cky <cleitonrocha321@hotmail.com>
2024-07-16 14:00:35 -0300

    add conditional render on toner page

--- reactjs-frontend/src/pages/toners/Toner.jsx
+++ reactjs-frontend/src/pages/toners/Toner.jsx
@@ -1,9 +1,17 @@
+import { useContext } from "react";
 import { Footer } from "../../components/footer/Footer";
 import { Header } from "../../components/header/Header";
+import { TonerContext } from "../../context/TonerContext";
 import { TonerStyled } from "./Styled";
 import { TonerList } from "./tonerList/TonerList";
+import { ModalTonerEdit } from "../../modals/toners/modalTonerEdit/ModalTonerEdit";
+import { ModalTonerDelete } from "../../modals/toners/modalTonerDelete/ModalTonerDelete";
+import { ModalTonerCreate } from "../../modals/toners/modalTonerCreate/ModalTonerCreate";
 
 export const Toner = () => {
+  const { setModalCreateIsOpen, modalDeleteIsOpen, modalEditIsOpen } =
+    useContext(TonerContext);
+
   return (
     <TonerStyled>
       <Header />
@@ -12,33 +20,12 @@ export const Toner = () => {
           Inserir Toner
         </span>
       </div>
-      {/* <div className="select-container">
-        <select name="" id="" className="select-toner">
-          <option value="" className="option-toner">
-            W9037MC
-          </option>
-          <option value="">203U</option>
-          <option value="">358S</option>
-          <option value="">W9004mz</option>
-          <option value="">C506L</option>
-          <option value="">Y506L</option>
-          <option value="">W9061MC</option>
-          <option value="">W9008MZ</option>
-          <option value="">W9063</option>
-          <option value="">M506L</option>
-          <option value="">K506L</option>
-          <option value="">W9060MC</option>
-          <option value="">W9062MC</option>
-          <option value="">201L</option>
-          <option value="">R304</option>
-          <option value="">304E</option>
-          <option value="">307U</option>
-          <option value="">309E</option>
-        </select>
-      </div>
-      <div className="body-toner"></div> */}
+
       <TonerList />
       <Footer />
+      {setModalCreateIsOpen ? <ModalTonerCreate /> : null}
+      {modalDeleteIsOpen ? <ModalTonerDelete /> : null}
+      {modalEditIsOpen ? <ModalTonerEdit /> : null}
     </TonerStyled>
   );
 };
