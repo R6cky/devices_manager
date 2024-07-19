0e791a2d6e73d2a0ec488b0dcf7b07b748529d6d
r6cky <cleitonrocha321@hotmail.com>
2024-07-18 23:07:39 -0300

    change styled of computer items

--- reactjs-frontend/src/pages/computer/computerList/computerItem/Styled.js
+++ reactjs-frontend/src/pages/computer/computerList/computerItem/Styled.js
@@ -5,30 +5,63 @@ export const ComputerItemStyled = styled.li`
   width: 21rem;
   height: 28rem;
   max-width: 100%;
-  border: solid red;
   padding: 5px;
   margin: 20px;
+  background: rgb(2, 0, 36);
+  padding: 20px;
+  -webkit-box-shadow: 4px 14px 26px 0px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 4px 14px 26px 0px rgba(0, 0, 0, 0.75);
+  box-shadow: 4px 14px 26px 0px rgba(0, 0, 0, 0.75);
+  animation: myAnim 2s ease 0s 1 normal forwards;
 
   .item-content {
-    border: solid green;
+    border: solid white 0.5px;
+    border-radius: 8px;
+    padding: 10px;
     height: 70%;
     margin-bottom: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
 
     .item-content-hostname {
+      background-color: yellow;
+      color: black;
+      padding: 5px;
+      border-radius: 8px;
+      width: 100%;
     }
 
     .item-content-ip {
+      background-color: yellow;
+      color: black;
+      padding: 5px;
+      border-radius: 8px;
+      width: 100%;
     }
     .item-content-sector {
+      background-color: yellow;
+      color: black;
+      padding: 5px;
+      border-radius: 8px;
+      width: 100%;
     }
 
     .item-content-type {
-      border: solid red;
+      background-color: yellow;
+      color: black;
+      padding: 5px;
+      border-radius: 8px;
       width: 100%;
-      height: 200px;
-      overflow-x: hidden;
-      display: flex;
-      flex-wrap: wrap;
+    }
+
+    .item-content-os {
+      background-color: yellow;
+      color: black;
+      padding: 5px;
+      border-radius: 8px;
+      width: 100%;
     }
   }
 
@@ -45,6 +78,16 @@ export const ComputerItemStyled = styled.li`
     .remove {
       width: 5rem;
       height: 3rem;
+    }
+  }
+
+  @keyframes myAnim {
+    0% {
+      opacity: 0;
+    }
+
+    100% {
+      opacity: 1;
     }
   }
 `;
