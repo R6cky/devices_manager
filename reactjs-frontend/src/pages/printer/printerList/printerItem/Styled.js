558255ee9db84fc6421bf0dcdcf301d0eb367600
r6cky <cleitonrocha321@hotmail.com>
2024-07-18 21:42:05 -0300

    change styles of printer items

--- reactjs-frontend/src/pages/printer/printerList/printerItem/Styled.js
+++ reactjs-frontend/src/pages/printer/printerList/printerItem/Styled.js
@@ -5,21 +5,65 @@ export const PrinterItemStyled = styled.li`
   width: 21rem;
   height: 28rem;
   max-width: 100%;
-  border: solid red;
   padding: 5px;
   margin: 20px;
+  color: white;
+  background: rgb(2, 0, 36);
+  padding: 20px;
+
+  -webkit-box-shadow: 4px 14px 26px 0px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 4px 14px 26px 0px rgba(0, 0, 0, 0.75);
+  box-shadow: 4px 14px 26px 0px rgba(0, 0, 0, 0.75);
 
   .item-content {
-    border: solid green;
+    border: solid white 0.5px;
+    border-radius: 8px;
+    padding: 10px;
     height: 70%;
     margin-bottom: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
 
     .item-content-hostname {
+      background-color: yellow;
+      color: black;
+      padding: 5px;
+      border-radius: 8px;
     }
 
     .item-content-ip {
+      background-color: yellow;
+      color: black;
+      padding: 5px;
+      border-radius: 8px;
     }
     .item-brand-and-model {
+      background-color: yellow;
+      color: black;
+      padding: 5px;
+      border-radius: 8px;
+    }
+
+    .item-toner-name {
+      background-color: yellow;
+      color: black;
+      padding: 5px;
+      border-radius: 8px;
+    }
+    .item-queue-name {
+      background-color: yellow;
+      color: black;
+      padding: 5px;
+      border-radius: 8px;
+    }
+
+    .item-sector {
+      background-color: yellow;
+      color: black;
+      padding: 5px;
+      border-radius: 8px;
     }
   }
 
