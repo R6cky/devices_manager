7d05d23f9df48a3e822b15967bd76514c2ba38d5
r6cky <cleitonrocha321@hotmail.com>
2024-07-23 21:30:52 -0300

    add postgres conection but has a bug

--- node-js_backend/src/database/prismaClient.js
+++ node-js_backend/src/database/prismaClient.js
@@ -1,5 +1,10 @@
 import { PrismaClient } from "@prisma/client";
+import pg from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const prismaClient = new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL + "?sslmode=require"}`;
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prismaClient = new PrismaClient({ adapter });
 
 export { prismaClient };
