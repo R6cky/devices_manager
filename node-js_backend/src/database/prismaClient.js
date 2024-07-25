f6d57233bb487aee5c09d92ce4e3e53cbaf39f42
r6cky <cleitonrocha321@hotmail.com>
2024-07-25 19:37:05 -0300

    fix bug related the conection between prisma and remote postgres

--- node-js_backend/src/database/prismaClient.js
+++ node-js_backend/src/database/prismaClient.js
@@ -2,9 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import pg from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = `${process.env.DATABASE_URL + "?sslmode=require"}`;
-const pool = new pg.Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-const prismaClient = new PrismaClient({ adapter });
+const connectionString = `${process.env.DATABASE_URL}`;
+const prismaClient = new PrismaClient({ datasourceUrl: connectionString });
 
 export { prismaClient };
