dbe56c493bcbd18f3b005ce217d8630cce144980
r6cky <cleitonrocha321@hotmail.com>
2024-08-22 15:02:06 -0300

    fix bug in DATABASE URL string

--- node-js_backend/src/controllers/users/getUserByEmail.js
+++ node-js_backend/src/controllers/users/getUserByEmail.js
@@ -1,9 +1,11 @@
 import { prismaClient } from "../../database/prismaClient.js";
 
 const getUserByEmail = (email) => {
-  return prismaClient.user.findUnique({
-    where: { email },
-  });
+  return (
+    prismaClient.user.findUnique({
+      where: { email },
+    }) || null
+  );
 };
 
 export { getUserByEmail };
