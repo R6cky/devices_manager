a3ae9635e225a85dfefdc07779a97c839231b80a
r6cky <cleitonrocha321@hotmail.com>
2024-10-19 11:44:44 -0300

    change value of variable user

--- reactjs-frontend/src/utils/ProtectRouter.jsx
+++ reactjs-frontend/src/utils/ProtectRouter.jsx
@@ -1,7 +1,7 @@
 import { Outlet, Navigate } from "react-router-dom";
 
 export const ProtectedRoutes = () => {
-  const user = true;
+  const user = null;
 
   return user ? <Outlet /> : <Navigate to="login" />;
 };
