75a100d4db6e6d500319a7c87a3d4e0a9070373a
r6cky <cleitonrocha321@hotmail.com>
2024-11-01 11:18:17 -0300

    adjust styles in Footer component

--- reactjs-frontend/src/components/footer/Styled.js
+++ reactjs-frontend/src/components/footer/Styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const FooterStyled = styled.footer`
   width: 100vw;
-  height: 300px;
+  height: 60px;
   display: flex;
   align-items: center;
   justify-content: center;
