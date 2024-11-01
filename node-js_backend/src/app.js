d50e4b63b81579b5feb087dc7164f77c198bd911
r6cky <cleitonrocha321@hotmail.com>
2024-11-01 18:34:44 -0300

    fix bug from CORS error

--- node-js_backend/src/app.js
+++ node-js_backend/src/app.js
@@ -2,7 +2,6 @@ import { router } from "./router.js";
 import express from "express";
 import cors from "cors";
 
-app.use(cors());
 const port = 3000;
 
 const app = express();
@@ -10,6 +9,13 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  app.use(cors());
+  next();
+});
 
 app.listen(`${port}`, () => {
   console.log(`Running on port ${port}`);
