0cbdfebe944f6ed71ac0f195a6682bf8a884c87e
r6cky <cleitonrocha321@hotmail.com>
2024-11-01 18:30:13 -0300

    swap cors call of place in file

--- node-js_backend/src/app.js
+++ node-js_backend/src/app.js
@@ -2,14 +2,14 @@ import { router } from "./router.js";
 import express from "express";
 import cors from "cors";
 
+const port = 3000;
+
 const app = express();
 
 app.use(express.json());
 
 app.use(router);
 app.use(cors());
-
-const port = 3000;
 
 app.listen(`${port}`, () => {
   console.log(`Running on port ${port}`);
