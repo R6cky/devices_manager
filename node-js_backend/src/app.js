b3dcc66860acd7e673d43751767ba2823289a09c
r6cky <cleitonrocha321@hotmail.com>
2024-11-02 09:13:43 -0300

    try fix bug related cors erro - comment function allow headers and set app.use(cors()) in top file

--- node-js_backend/src/app.js
+++ node-js_backend/src/app.js
@@ -5,16 +5,15 @@ import cors from "cors";
 const port = 3000;
 
 const app = express();
+app.use(cors());
 
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//   app.use(cors());
+//   next();
+// });
 app.use(express.json());
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  app.use(cors());
-  next();
-});
-
 app.use(router);
 app.listen(`${port}`, () => {
   console.log(`Running on port ${port}`);
