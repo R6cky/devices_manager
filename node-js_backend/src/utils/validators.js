88ebdddeef3fe729460957697174e6ebef25279f
r6cky <cleitonrocha321@hotmail.com>
2024-11-04 09:19:32 -0300

    create validation schemas of create toner, ...printer, ...computer

--- node-js_backend/src/utils/validators.js
+++ node-js_backend/src/utils/validators.js
@@ -33,3 +33,25 @@ export const schemaCreateTas = z.object({
   hostname: z.string("O valor deve ser texto"),
   wifi_name: schemaCreateBluebids.string("O valor deve ser texto"),
 });
+
+export const schemaCreateToner = z.object({
+  model: z.string("O valor deve ser texto"),
+  quantity: z.number("O valor deve ser número"),
+});
+
+export const schemaCreatePrinter = z.object({
+  hostname: z.string("O valor deve ser texto."),
+  ip: z.string("O valor deve ser texto."),
+  brand_and_model: z.string("O valor deve ser texto."),
+  queue_name: z.string("O valor deve ser texto."),
+  sector: z.string("O valor deve ser texto."),
+  toner_name: z.string("O valor deve ser texto."),
+});
+
+export const schemaCreateComputer = z.object({
+  hosthname: z.string("O valor deve ser texto"),
+  ip: z.string("O valor deve ser texto"),
+  sector: z.string("O valor deve ser texto"),
+  operational_system: z.string(""),
+  type: z.string("O valor deve ser texto"),
+});
