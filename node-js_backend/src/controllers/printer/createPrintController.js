c0273ab1040243d2723f682ba30adbfeb22c0890
r6cky <cleitonrocha321@hotmail.com>
2024-11-04 15:11:39 -0300

    add schema validation in create and update  printer

--- node-js_backend/src/controllers/printer/createPrintController.js
+++ node-js_backend/src/controllers/printer/createPrintController.js
@@ -1,20 +1,17 @@
 import { prismaClient } from "../../database/prismaClient.js";
+import { schemaCreatePrinter } from "../../utils/validators.js";
 
 export class CreatePrintController {
   async handle(req, res) {
-    const { hostname, ip, brand_and_model, queue_name, sector, toner_name } =
-      req.body;
-
-    const print = await prismaClient.printer.create({
-      data: {
-        hostname,
-        ip,
-        brand_and_model,
-        queue_name,
-        sector,
-        toner_name,
-      },
-    });
-    return res.status(201).json(print);
+    try {
+      const dataPrinter = schemaCreatePrinter.parse(req.body);
+      const print = await prismaClient.printer.create({
+        data: dataPrinter,
+      });
+      return res.status(201).json(print);
+    } catch (error) {
+      console.log(error.issues);
+      return res.status(400).json(error.issues);
+    }
   }
 }
--- node-js_backend/src/controllers/printer/updatePrintController.js
+++ node-js_backend/src/controllers/printer/updatePrintController.js
@@ -1,20 +1,20 @@
 import { prismaClient } from "../../database/prismaClient.js";
+import { schemaCreatePrinter } from "../../utils/validators.js";
 
 export class UpdatePrintController {
   async handle(req, res) {
-    const printer = await prismaClient.printer.update({
-      where: {
-        id: req.params.id,
-      },
-      data: {
-        hostname: req.body.hostname,
-        ip: req.body.ip,
-        brand_and_model: req.body.brand_and_model,
-        queue_name: req.body.queue_name,
-        sector: req.body.sector,
-        toner_name: req.body.toner_name,
-      },
-    });
-    return res.status(200).json(printer);
+    try {
+      const dataPrinter = schemaCreatePrinter.partial(req.body);
+      const printer = await prismaClient.printer.update({
+        where: {
+          id: req.params.id,
+        },
+        data: dataPrinter,
+      });
+      return res.status(200).json(printer);
+    } catch (error) {
+      console.log(error.issues);
+      res.status(200).json(error.issues);
+    }
   }
 }
