b863efe18f0f5c5597b0aeed7ef79ec0a1604cb3
r6cky <cleitonrocha321@hotmail.com>
2024-11-04 15:00:14 -0300

    add schema validation in update  toner

--- node-js_backend/src/controllers/toner/updateTonerController.js
+++ node-js_backend/src/controllers/toner/updateTonerController.js
@@ -1,18 +1,20 @@
 import { prismaClient } from "../../database/prismaClient.js";
+import { schemaCreateToner } from "../../utils/validators.js";
 
 export class UpdateTonerController {
   async handle(req, res) {
-    const { model, quantity } = req.body;
-
-    const toner = await prismaClient.toner.update({
-      where: {
-        id: req.params.id,
-      },
-      data: {
-        model,
-        quantity,
-      },
-    });
-    return res.status(200).json(toner);
+    try {
+      const dataToner = schemaCreateToner.partial(req.body);
+      const toner = await prismaClient.toner.update({
+        where: {
+          id: req.params.id,
+        },
+        data: dataToner,
+      });
+      return res.status(200).json(toner);
+    } catch (error) {
+      console.log(error.issues);
+      return res.status(400).json(error.issues);
+    }
   }
 }
