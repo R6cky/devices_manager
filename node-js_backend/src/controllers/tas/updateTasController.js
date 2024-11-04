324f2cfef5732aea126699673f006f1ff2865551
r6cky <cleitonrocha321@hotmail.com>
2024-11-04 14:55:01 -0300

    add schema validation in update  tas

--- node-js_backend/src/controllers/tas/updateTasController.js
+++ node-js_backend/src/controllers/tas/updateTasController.js
@@ -1,16 +1,19 @@
 import { prismaClient } from "../../database/prismaClient.js";
-
+import { schemaCreateTas } from "../../utils/validators.js";
 export class UpdateTasController {
   async handle(req, res) {
-    const tas = await prismaClient.tas.update({
-      where: {
-        id: req.params.id,
-      },
-      data: {
-        hostname: req.body.hostname,
-        wifi_name: req.body.wifi_name,
-      },
-    });
-    return res.status(200).json(tas);
+    try {
+      const dataTas = schemaCreateTas.partial(req.body);
+      const tas = await prismaClient.tas.update({
+        where: {
+          id: req.params.id,
+        },
+        data: dataTas,
+      });
+      return res.status(200).json(tas);
+    } catch (error) {
+      console.log(error.issues);
+      return res.status(400).json(error.issues);
+    }
   }
 }
