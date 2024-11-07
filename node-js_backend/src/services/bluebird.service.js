2d26863a747f4301ebcbf756afe0e78c155f4f59
r6cky <cleitonrocha321@hotmail.com>
2024-11-07 18:40:42 -0300

    remove trycat from bluebird services

--- node-js_backend/src/services/bluebird.service.js
+++ node-js_backend/src/services/bluebird.service.js
@@ -2,41 +2,28 @@ import { prismaClient } from "../database/prismaClient.js";
 import { schemaCreateBluebids } from "../utils/validators.js";
 
 export const createBluebirdService = async (data) => {
-  try {
-    const dataBluebird = schemaCreateBluebids.parse(data);
-    const bluebird = await prismaClient.blueBird.create({
-      data: dataBluebird,
-    });
-
-    return bluebird;
-  } catch (error) {
-    console.log(error);
-  }
+  const dataBluebird = schemaCreateBluebids.parse(data);
+  const bluebird = await prismaClient.blueBird.create({
+    data: dataBluebird,
+  });
+  return bluebird;
 };
 
 export const deleteBluebirdService = async (id) => {
-  try {
-    const bluebird = await prismaClient.blueBird.delete({
-      where: {
-        id: id,
-      },
-    });
-    return bluebird;
-  } catch (error) {
-    console.log(error);
-  }
+  const bluebird = await prismaClient.blueBird.delete({
+    where: {
+      id: id,
+    },
+  });
+  return bluebird;
 };
 
 export const getAllBluebirdService = async () => {
-  try {
-    const bluebirds = await prismaClient.blueBird.findMany();
-    return bluebirds;
-  } catch (error) {
-    console.log(error);
-  }
+  const bluebirds = await prismaClient.blueBird.findMany();
+  return bluebirds;
 };
 
-export const getBluebirdByIdService = (id) => {
+export const getBluebirdByIdService = async (id) => {
   const bluebirds = prismaClient.blueBird.findUnique({
     where: {
       id: id,
@@ -45,7 +32,7 @@ export const getBluebirdByIdService = (id) => {
   return bluebirds;
 };
 
-export const updateBluebirdService = (data, id) => {
+export const updateBluebirdService = async (data, id) => {
   const dataBluebird = schemaCreateBluebids.parse(data);
   const bluebird = prismaClient.blueBird.update({
     data: dataBluebird,
