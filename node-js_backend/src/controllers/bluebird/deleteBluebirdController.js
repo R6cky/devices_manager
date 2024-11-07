ef6f8a0e176da5bb1a762450e0783dc1ad0731ea
r6cky <cleitonrocha321@hotmail.com>
2024-11-07 18:33:41 -0300

    add trycat in bluebird controller function

--- node-js_backend/src/controllers/bluebird/deleteBluebirdController.js
+++ node-js_backend/src/controllers/bluebird/deleteBluebirdController.js
@@ -1,6 +1,11 @@
 import { deleteBluebirdService } from "../../services/bluebird.service.js";
 
 export const deleteBluebirdController = (req, res) => {
-  deleteBluebirdService(req.params.id);
-  return res.status(204).json({ message: "Deletado com sucesso!" });
+  try {
+    deleteBluebirdService(req.params.id);
+    return res.status(204).json({ message: "Deletado com sucesso!" });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error);
+  }
 };
