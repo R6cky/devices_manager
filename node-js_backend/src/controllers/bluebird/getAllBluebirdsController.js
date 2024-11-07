5e417679f3e1a708dd368dbbaa0991e5944a5e2f
r6cky <cleitonrocha321@hotmail.com>
2024-11-07 18:35:22 -0300

    add trycat in bluebird controller function

--- node-js_backend/src/controllers/bluebird/getAllBluebirdsController.js
+++ node-js_backend/src/controllers/bluebird/getAllBluebirdsController.js
@@ -1,7 +1,12 @@
 import { getAllBluebirdService } from "../../services/bluebird.service.js";
 
 export const getAllBluebirdsController = async (req, res) => {
-  const bluebirds = await getAllBluebirdService();
-  console.log(bluebirds);
-  return res.status(200).json(bluebirds);
+  try {
+    const bluebirds = await getAllBluebirdService();
+    console.log(bluebirds);
+    return res.status(200).json(bluebirds);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error);
+  }
 };
--- node-js_backend/src/controllers/bluebird/getBluebirdByIdController.js
+++ node-js_backend/src/controllers/bluebird/getBluebirdByIdController.js
@@ -1,6 +1,11 @@
 import { getBluebirdByIdService } from "../../services/bluebird.service.js";
 
 export const getBluebirdByIdController = async (req, res) => {
-  const bluebird = await getBluebirdByIdService(req.params.id);
-  return res.status(200).json(bluebird);
+  try {
+    const bluebird = await getBluebirdByIdService(req.params.id);
+    return res.status(200).json(bluebird);
+  } catch (error) {
+    console.log(error);
+    return res.satus(400).json(error);
+  }
 };
