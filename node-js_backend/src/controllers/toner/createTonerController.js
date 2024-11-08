f3ecf02e38915b8593643e35d912ddbcb7591b3f
r6cky <cleitonrocha321@hotmail.com>
2024-11-07 21:39:00 -0300

    fix bug in router file

--- node-js_backend/src/controllers/toner/createTonerController.js
+++ node-js_backend/src/controllers/toner/createTonerController.js
@@ -1,6 +1,6 @@
 import { createTonerService } from "../../services/toner.services.js";
 
-export const CreateTonerController = async (req, res) => {
+export const createTonerController = async (req, res) => {
   try {
     const toner = createTonerService(req.body);
     return res.status(201).json(toner);
