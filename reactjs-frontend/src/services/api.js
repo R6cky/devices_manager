4c9d71ce184b2492b5a779253c3cd7e43dbb163b
r6cky <cleitonrocha321@hotmail.com>
2024-11-08 10:06:00 -0300

    swap name from user register function

--- reactjs-frontend/src/services/api.js
+++ reactjs-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://devices-manager.onrender.com",
+  baseURL: "https://devices-managerr.onrender.com",
   //baseURL: "http://localhost:3000",
   timeout: 15000,
 });
