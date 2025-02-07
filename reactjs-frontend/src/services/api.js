915550e047b34ad2fff69ce53a32ff614c245c9f
r6cky <cleitonrocha321@hotmail.com>
2025-02-07 08:05:18 -0300

    fix bug in api running local

--- reactjs-frontend/src/services/api.js
+++ reactjs-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://devices-managerr.onrender.com",
-  //baseURL: "http://localhost:3000",
+  //baseURL: "https://devices-managerr.onrender.com",
+  baseURL: "http://localhost:3001",
   timeout: 15000,
 });
